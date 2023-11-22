3665e6eda337e484b08716bcdbea14ff50e81342
Fredtorn <fredrika.tornkvist@chasacademy.se>
2023-11-22 11:19:47 +0100

    added comments

--- code/radioplayer.js
+++ code/radioplayer.js
@@ -8,7 +8,7 @@
 // </audio>
 
 
-const mainContainerEl = document.getElementById("mainContainer");
+const mainContainerEl = document.getElementById("mainContainer");   // Skapar en variabel som hämtar mainContainer-diven från HTML (föräldern)
 
 fetch("https://api.sr.se/api/v2/channels/?format=json")
     .then((response) => {
@@ -21,7 +21,7 @@ fetch("https://api.sr.se/api/v2/channels/?format=json")
 
         const myData = data;  // Samlar all data från json i en variabel.
 
-        myData.channels.forEach((channel) => {  //Skapar en funktion som hämtar
+        myData.channels.forEach((channel) => {  //Skapar en funktion som hämtar datan från variabeln, från arraysen channels, och loopar igenom varje array.
 
             const bodyEl = document.createElement("div")
             const imgEl = document.createElement("img")
@@ -30,19 +30,17 @@ fetch("https://api.sr.se/api/v2/channels/?format=json")
             const audioSourceEl = document.createElement("source")
 
             bodyEl.setAttribute("class", "mainBody")
-            imgEl.setAttribute("src", `${channel.image}`)
+            imgEl.setAttribute("src", `${channel.image}`)  // hämtar attribut från json i arrow-funktionen via channel och sen image.
             imgEl.setAttribute("alt", `${channel.name}`)
             titleEl.setAttribute("class", "title")
             audioEl.setAttribute("class", "audioEl")
             audioSourceEl.setAttribute("src", `${channel.liveaudio.url}`)
             audioSourceEl.setAttribute("type", "audio/mpeg")
-            audioSourceEl.setAttribute("type", "audio/ogg")
 
+            audioEl.controls = true;                                    // Skapar ljudkontrollerna till spelaren och sätter den som true.
 
-            audioEl.controls = true;
-
-            titleEl.textContent = channel.name;
-            bodyEl.style.backgroundColor = `#${channel.color}`
+            titleEl.textContent = channel.name;                         // Lägger in hämtad text från json i div titelEl
+            bodyEl.style.backgroundColor = `#${channel.color}`          // Lägger till backgroundColor i bodyEls style.css
 
             mainContainerEl.appendChild(bodyEl);
             bodyEl.appendChild(imgEl);
